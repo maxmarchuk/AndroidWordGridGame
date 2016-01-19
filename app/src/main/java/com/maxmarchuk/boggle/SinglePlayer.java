83809403ceeec870c3fe0e3c7618776a8ba000d6
Max Marchuk <maxmarchuk@gmail.com>
2016-01-19 11:17:55 -0800

    changed color scheme for main menu

--- app/src/main/java/com/maxmarchuk/boggle/SinglePlayer.java
+++ app/src/main/java/com/maxmarchuk/boggle/SinglePlayer.java
@@ -14,7 +14,7 @@ public class SinglePlayer extends AppCompatActivity {
 
     GridView letterGrid;
 
-    static final String[] letters = new String[] {
+    static final String[] letters = new String[]{
             "A", "B", "C", "D",
             "E", "F", "G", "H",
             "I", "J", "K", "L",
@@ -25,20 +25,8 @@ public class SinglePlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_player);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, letters);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, letters);
 
         letterGrid = (GridView) findViewById(R.id.gridView);
         letterGrid.setBackgroundColor(Color.parseColor("#a7a7a7a7"));
@@ -52,6 +40,7 @@ public class SinglePlayer extends AppCompatActivity {
 
         //
     }
+
     private void populateGrid() {
         // import the file?
 
