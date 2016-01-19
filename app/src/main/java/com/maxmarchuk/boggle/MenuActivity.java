5afdc6e29ef4c42714e603a18e871978a6ad54bc
Max Marchuk <maxmarchuk@gmail.com>
2016-01-18 21:59:41 -0800

    Added a basic 4 grid of letters

--- app/src/main/java/com/maxmarchuk/boggle/MenuActivity.java
+++ app/src/main/java/com/maxmarchuk/boggle/MenuActivity.java
@@ -1,5 +1,7 @@
 package com.maxmarchuk.boggle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +11,26 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MenuActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -48,5 +53,50 @@ public class MenuActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToSinglePlayer(View view) {
+        Intent intent = new Intent(MenuActivity.this, SinglePlayer.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Menu Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.maxmarchuk.boggle/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Menu Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.maxmarchuk.boggle/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- app/src/main/java/com/maxmarchuk/boggle/SinglePlayer.java
+++ app/src/main/java/com/maxmarchuk/boggle/SinglePlayer.java
@@ -1,13 +1,25 @@
 package com.maxmarchuk.boggle;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class SinglePlayer extends AppCompatActivity {
+
+    GridView letterGrid;
+
+    static final String[] letters = new String[] {
+            "A", "B", "C", "D",
+            "E", "F", "G", "H",
+            "I", "J", "K", "L",
+            "M", "N", "O", "P"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,39 @@ public class SinglePlayer extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, letters);
+
+        letterGrid = (GridView) findViewById(R.id.gridView);
+        letterGrid.setBackgroundColor(Color.parseColor("#a7a7a7a7"));
+        letterGrid.setAdapter(adapter);
+    }
+
+    private void startGame() {
+        // start the timer
+
+        populateGrid();
+
+        //
+    }
+    private void populateGrid() {
+        // import the file?
+
+        // Run the algorithm to return a 4x4 grid of letters
+
+        // populate the GridView (letterGrid) with the letters
+    }
+
+
+    // Start the game timer
+    private void startTimer() {
+
+    }
+
+    // Stop the game timer
+    private void stopTimer() {
+
     }
 
 }
