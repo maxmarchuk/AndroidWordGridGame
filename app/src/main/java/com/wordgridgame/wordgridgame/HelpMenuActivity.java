5b5cedc57f56e4e49a7f5b58c8a2f2f7cd3ca710
Mahija Daliparthi <dmahija@pdx.edu>
2016-02-17 11:04:26 -0800

    Merge branch 'master' of https://projects.cecs.pdx.edu/git/havlicek-cs454t4
    # Conflicts:
    #	app/src/main/java/com/wordgridgame/wordgridgame/SinglePlayerActivity.java

--- app/src/main/java/com/wordgridgame/wordgridgame/HelpMenuActivity.java
+++ app/src/main/java/com/wordgridgame/wordgridgame/HelpMenuActivity.java
@@ -2,8 +2,10 @@ package com.wordgridgame.wordgridgame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.wordgridgame.wordgridgame.R.layout.activity_help_menu;
 
@@ -14,9 +16,16 @@ public class HelpMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_help_menu);
+        String fontPath = "fonts/fonts.ttf";
+        TextView helpTitle = (TextView) findViewById(R.id.txtBoggle);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        helpTitle.setTypeface(tf);
+
     }
     public void goToMainMenuActivity(View view){
         Intent intent=new Intent(getApplicationContext(),MainMenuActivity.class);
         startActivity(intent);
     }
+
 }
