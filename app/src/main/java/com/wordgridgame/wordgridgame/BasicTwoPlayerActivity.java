7e24daa1326ffb901d757731778dd6d71ff84d45
Max Marchuk <maxmarchuk@gmail.com>
2016-02-23 15:53:30 -0800

    Create Base for basic two player mode

---
+++ app/src/main/java/com/wordgridgame/wordgridgame/BasicTwoPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.wordgridgame.wordgridgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicTwoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_two_player);
+    }
+}
