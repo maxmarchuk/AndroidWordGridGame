484195be61dab721d5904a4dd44ba32ac1f424c9
Max Marchuk <maxmarchuk@gmail.com>
2016-03-07 20:03:58 -0800

    added multiround activity templates n fixed things

---
+++ app/src/main/java/com/wordgridgame/wordgridgame/MultiroundClientActivity.java
@@ -0,0 +1,13 @@
+package com.wordgridgame.wordgridgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MultiroundClientActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiround_client);
+    }
+}
